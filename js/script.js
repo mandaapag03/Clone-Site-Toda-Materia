3efce6d9aed2ac1e6f8e0ecf78c73db1c2cc8378
mandaapag03 <paganiamanda791@gmail.com>
2022-11-09 19:31:01 -0300

    add script button 'ver resposta'

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-function mostrarEsconderResposta() { }
+function mostrarEsconderDiv(botao, div_displayNone) { 
+    if(div_displayNone.style.display === "none"){
+       div_displayNone.style.display = "block";
+       botao.style.backgroundColor = "#A3C2DA";
+       botao.style.color = '#000';
+       botao.innerHTML = 'Esconder Resposta';
+    } else{
+        div_displayNone.style.display = "none";
+        botao.style.backgroundColor = "#005B9C";
+        botao.style.color = '#FFF';
+        botao.innerHTML = 'Ver Resposta';
+    }
+}
 
-let botaoResposta = document.querySelector('.btn-resposta')
+let botaoResposta = document.getElementsByClassName('.btn-resposta');
 console.log(botaoResposta)
 
 botaoResposta.addEventListener("click", function () {
-
+    let textResposta = document.getElementsByClassName('.text-resposta');
+    mostrarEsconderDiv(botaoResposta, textResposta);
 })
