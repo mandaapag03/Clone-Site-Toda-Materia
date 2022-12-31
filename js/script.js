483fab14ff82fdb304a47bebd99e52e78543921c
mandaapag03 <paganiamanda791@gmail.com>
2022-12-31 15:20:13 -0300

    Finaliza projeto

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-function mostrarEsconderDiv(botao, div_displayNone) { 
-    if(div_displayNone.style.display === "none"){
-       div_displayNone.style.display = "block";
-       botao.style.backgroundColor = "#A3C2DA";
-       botao.style.color = '#000';
-       botao.innerHTML = 'Esconder Resposta';
-    } else{
-        div_displayNone.style.display = "none";
+function mostrarEsconderDiv(botao, textoResposta) {
+    if (textoResposta.style.display === "none") {
+        textoResposta.style.display = "block";
+        botao.style.backgroundColor = "#A3C2DA";
+        botao.style.color = '#000';
+        botao.innerHTML = 'Esconder Resposta';
+    } else {
+        textoResposta.style.display = "none";
         botao.style.backgroundColor = "#005B9C";
         botao.style.color = '#FFF';
         botao.innerHTML = 'Ver Resposta';
     }
 }
 
-let botaoResposta = document.getElementsByClassName('.btn-resposta');
-console.log(botaoResposta)
+let botoesResposta = document.querySelectorAll(".btn-resposta");
+let textosResposta = document.querySelectorAll('.text-resposta');
 
-botaoResposta.addEventListener("click", function () {
-    let textResposta = document.getElementsByClassName('.text-resposta');
-    mostrarEsconderDiv(botaoResposta, textResposta);
-})
+for (let i = 0; i < botoesResposta.length; i++) {
+    botoesResposta[i].addEventListener("click", function () {
+        mostrarEsconderDiv(this, textosResposta[i]);
+    })
+}
